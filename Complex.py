b2b5ca0b8ae2b55509e1fbacc2bdfb5be8a08657
Michal Piotr Szmigiel <mp.szmigiel@gmail.com>
2014-07-24 17:47:41 +0200

    Update Complex.py

--- Complex.py
+++ Complex.py
@@ -1,37 +1,41 @@
-import math
+#Michał Piotr Szmigiel
+#24 lipca 2014
+#Python 3.3.0
+#Funkcja zespolona w Pythonie
+import math                                             #importowanie pakietu math
 
-def ComplexNumber(re, im):
-    re = float(re)
-    im = float(im)
-    if re == 0 and im >=0:
-        print('%s i' %(im))
-    elif re == 0 and im < 0:
-        print('%s i' %(im))
-    elif re > 0 and im < 0:
-        print('%s %s i' %(re, im))
-    elif re < 0 and im < 0:
-        print('%s %s i' %(re, im))
+def ComplexNumber(re, im):                              #Tworzenie funkcji imaginarisa.
+    re = float(re)                                      #Zmienne realis i imaginaris są równe typom float 
+    im = float(im)                                      #czyli typom rzeczywistym.
+    if re == 0 and im >=0:                              #Jeżeli realis jest równy 0 i imaginaris większy lub
+        print('Liczba zespolona : %si' %(im))           #równy 0 to przepisuje wynik wartości urojonej.
+    elif re == 0 and im < 0:                            #W innym przypadku jeżeli realis jest równy 0 i imaginaris 
+        print('Liczba zespolona : %si' %(im))           #mniejszy od 0 to przepisuje wynik wartości urojonej.         
+    elif re > 0 and im < 0:                             #W innym przypadku jeżeli realis jest wiekszy od 0 i imaginaris
+        print('Liczba zespolona : %s%si' %(re, im))     #mniejszy od 0 to przepisuje wynik re-im*i.
+    elif re < 0 and im < 0:                             #W innym przypadku jeżeli realis jest mniejszy od 0 i imaginaris
+        print('Liczba zespolona : %s%si' %(re, im))     #mniejszy od 0 to przepisuje wynik -re-im*i.
     else:
-        print('%s + %s i' %(re, im))
+        print('Liczba zespolona : %s+%s i' %(re, im))   #W ostatnim przypadku zwraca wartość re+im*i
 
 
-def modulus(re, im):
-    re = float(re)
+def Modulus(re, im):                                    #Tworzenie funkcji modułu zespolonego.
+    re = float(re)                                      #Zmienne realis i imaginaris są równe typom float.
     im = float(im)
-    return math.sqrt((re*re) + (im*im))
+    return math.sqrt((re*re) + (im*im))                 #Zwraca wynik znany ze stwierdzenia Pitagorasa sqrt(re^2 + im^2).
 
-def argument(re, im):
-    re = float(re)
+def Argument(re, im):                                   #Tworzenie funkcji argumentu.
+    re = float(re)                                      #Zmienne realis i imaginaris są równe typom float.
     im = float(im)
-    if re > 0:
+    if re > 0:                                          #Jeżeli realis > 0 to zwraca wynik arctan(re/im).                                
         return math.atan(im/re)
-    elif re < 0 and im >= 0:
+    elif re < 0 and im >= 0:                            #W innym przypadku jeżeli realis < 0 i imaginaris >= 0 to zwraca wynik arctan(re/im) + pi.
         return math.atan(im/re) + math.pi
-    elif re < 0 and im < 0:
+    elif re < 0 and im < 0:                             #W innym przypadku jeżeli realis < 0 i imaginaris < 0 to zwraca wynik arctan(re/im) - pi.
         return math.atan(im/re) - math.pi
-    elif re == 0 and im > 0:
+    elif re == 0 and im > 0:                            #W innym przypadku jeżeli realis = 0 i imaginaris > 0 to zwraca wynik pi/2.
         return math.pi/2
-    elif re == 0 and im < 0:
+    elif re == 0 and im < 0:                            #W innym przypadku jeżeli realis = 0 i imaginaris < 0 to zwraca wynik -pi/2.
         print('-%s'  %(math.pi/2))
-    else:
-        print('Independent')
+    else:                                               #W ostatnim przypadku wartość jest niezdefiniowana.
+        print('Independent')             
